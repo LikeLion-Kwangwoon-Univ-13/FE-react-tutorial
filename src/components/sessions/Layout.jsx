38a202ec45eedaf327f160274f9699a428436374
minhyeong <minhyeong.dev@gmail.com>
2025-04-03 16:18:51 +0900

    fix: fix layout

--- src/components/sessions/Layout.jsx
+++ src/components/sessions/Layout.jsx
@@ -8,6 +8,7 @@ export default function Layout() {
     displays: "flex flex-col gap-3.5",
     sizes: "w-full min-h-screen",
     paddings: "p-5",
+    styles: "overflow-y-scroll",
   };
   const titleBox = {
     displays: "flex flex-col gap-y-1.5 ",
@@ -18,7 +19,7 @@ export default function Layout() {
         <div className="text-4xl font-bold">{titles.title}</div>
         <div className="text-2xl">{titles.subtitle}</div>
       </div>
-      <div className="w-full min-h-[70vh] border-2 border-blue-500 p-3.5">
+      <div className="w-full h-[70vh] border-2 border-blue-500 p-3.5 overflow-sc">
         <Outlet />
       </div>
       <Controller />
